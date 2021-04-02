55c626971a5c9330b2a7decae7cb3c1164a2f644
FrancoRueta <askjfuhje@hotmail.com>
2021-04-02 14:52:32 -0300

    Agregada la API de inicio de sesion.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,8 @@ from rest_framework import filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -111,5 +113,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Maneja la creacion de tokens de autentificacion."""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
 
 
